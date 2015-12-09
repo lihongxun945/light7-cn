59ccb06f5fe83fcb5baa0022b520ef383a7f16e0
lihongxun945 <lihongxun945@gmail.com>
2015-12-09 10:25:06 +0800

    fix $.initPage bug

--- js/init.js
+++ js/init.js
@@ -11,7 +11,7 @@
 
   //初始化页面中的JS组件
   $.initPage = function(page) {
-    var $page = getPage();
+    var $page = page ? $(page) : getPage();
     if(!$page[0]) $page = $(document.body);
     var $content = $page.hasClass("content") ? $page : $page.find(".content");
     $content.scroller();  //注意滚动条一定要最先初始化
